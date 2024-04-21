2c1b2e6dadef970ea210e945d4793b206bd0d62c
Nikunj Gupta <nikunjgupta609@gmail.com>
2024-04-21 20:25:50 +0530

    Add files via upload

---
+++ connect/public_static/main.js
@@ -0,0 +1,122 @@
+/**
+ * Created by ParikanshAndAtikant on 16/07/2017.
+ */
+var roomname = prompt("Enter Room Name ");
+$(function () {
+
+    var socket = io();
+    socket.emit('subscribe',roomname);
+    var audio = document.getElementById('myaudio');
+    $('#load').click(function () {
+       audio.load();
+       $('#load').html('Loading...');
+    });
+    function loaded() {
+        $('#load').html('Loaded.');
+    }
+    audio.addEventListener('canplay',loaded,false);
+
+    $('#song0').click(function () {
+       $('#myaudio').html('<source src="despacito.mp3" class="audio-source">');
+       $('#load').click();
+       $('#pause').click();
+       socket.emit('song0',roomname);
+    });
+    $('#song1').click(function () {
+        $('#myaudio').html('<source src="cute.mp3" class="audio-source">');
+        $('#load').click();
+        $('#pause').click();
+        socket.emit('song1',roomname);
+    });
+    $('#song2').click(function () {
+        $('#myaudio').html('<source src="Tum-Se.mp3" class="audio-source">');
+        $('#load').click();
+        $('#pause').click();
+        socket.emit('song2',roomname);
+     });
+    $('#song3').click(function () {
+        $('#myaudio').html('<source src="As-It-Was.mp3" class="audio-source">');
+        $('#load').click();
+        $('#pause').click();
+        socket.emit('song3',roomname);
+    });
+    $('#play').click(function () {
+        console.log("play");
+        $('#play').removeClass('show');
+        $('#play').addClass('hide');
+        $('#pause').removeClass('hide');
+        $('#pause').addClass('show');
+        socket.emit("play", {times: audio.currentTime, room : roomname});
+    });
+    $('#pause').click(function () {
+        console.log("pause");
+        $('#pause').removeClass('show');
+        $('#pause').addClass('hide');
+        $('#play').removeClass('hide');
+        $('#play').addClass('show');
+        socket.emit("pause", {times: audio.currentTime, room : roomname});
+    });
+    socket.on("first", function (data) {
+        audio.ontimeupdate = function () {
+            socket.emit("where", {times: audio.currentTime, room : roomname});
+        };
+        /*$('#play').click(function () {
+            console.log("play");
+            $('#play').removeClass('show');
+            $('#play').addClass('hide');
+            $('#pause').removeClass('hide');
+            $('#pause').addClass('show');
+            socket.emit("play", audio.currentTime);
+        });
+        $('#pause').click(function () {
+            console.log("pause");
+            $('#pause').removeClass('show');
+            $('#pause').addClass('hide');
+            $('#play').removeClass('hide');
+            $('#play').addClass('show');
+            socket.emit("pause", audio.currentTime);
+        });*/
+    });
+    socket.on('song0',function (data) {
+       console.log(data);
+        $('#myaudio').html('<source src="despacito.mp3" class="audio-source">');
+        $('#load').click();
+    });
+    socket.on('song1',function (data) {
+        console.log(data);
+        $('#myaudio').html('<source src="cute.mp3" class="audio-source">');
+        $('#load').click();
+    });
+    socket.on('song2',function (data) {
+        console.log(data);
+        $('#myaudio').html('<source src="Tum-Se.mp3" class="audio-source">');
+        $('#load').click();
+    });
+    socket.on('song3',function (data) {
+        console.log(data);
+        $('#myaudio').html('<source src="As-It-Was.mp3" class="audio-source">');
+        $('#load').click();
+    });
+    socket.on("current", function (data) {
+        var diff = audio.currentTime - data;
+        if (diff < 0 || diff > 2) {
+            audio.currentTime = data;
+        }
+    });
+    socket.on("playsong", function (data) {
+        audio.currentTime = data;
+        audio.play();
+        $('#play').removeClass('show');
+        $('#play').addClass('hide');
+        $('#pause').removeClass('hide');
+        $('#pause').addClass('show');
+    });
+    socket.on("pausesong", function (data) {
+        audio.currentTime = data;
+        audio.pause();
+        $('#pause').removeClass('show');
+        $('#pause').addClass('hide');
+        $('#play').removeClass('hide');
+        $('#play').addClass('show');
+    });
+});
---
+++ connect/server.js
@@ -0,0 +1,52 @@
+/**
+ * Created by ParikanshAndAtikant.
+ */
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var PORT = process.env.PORT || 5000;
+var ctr=0;
+
+
+/*app.get('/',function (req,res) {
+   res.send("Hello world!");
+});*/
+app.use('/',express.static('public_static'));
+
+io.on('connection',function (socket) {
+    console.log("connnected client");
+    if (ctr == 0) {
+        console.log("FirstPerson");
+        socket.emit("first", "first");
+    }
+    ctr++;
+    socket.on('subscribe',function (room) {
+       socket.join(room);
+    });
+    socket.on("disconnect", function () {
+        console.log("Client Disconnected");
+        ctr--;
+    });
+    socket.on('song0',function (data) {
+        console.log('changing to ' + data);
+        io.to(data).emit('song0','cute');
+    });
+    socket.on('song1',function (data) {
+        console.log('changing to ' + data);
+        io.to(data).emit('song1','desp');
+    });
+    socket.on("play",function (data) {
+       io.to(data.room).emit('playsong',data.times);
+    });
+    socket.on("pause",function (data) {
+        io.to(data.room).emit('pausesong',data.times);
+    });
+    socket.on("where",function (data) {
+        //console.log(data);
+        io.to(data.room).emit("current",data.times);
+    });
+});
+http.listen(PORT,function () {
+    console.log("server running on " + PORT);
+});
